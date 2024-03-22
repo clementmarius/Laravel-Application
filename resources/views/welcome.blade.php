86b7b57569c70e00727c70ac4b6ef1c4fe38f4f6
marius <mariusclement68@gmail.com>
2024-03-22 12:12:56 +0100

    install tailwind localement + modifiaction de package.json + ajout de node latest version (presion min 16) + modification de app.css  + ajout @vite sur le fichier welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    @vite('resources/css/app.css')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
     <title>Landing Page</title>
 </head>
 <body>
@@ -64,5 +65,6 @@
     </div>
 </section>
 
+{{--<script src="/resources/js/flowbite.js"></script>--}}
 </body>
 </html>
