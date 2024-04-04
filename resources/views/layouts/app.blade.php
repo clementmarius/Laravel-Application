c99ea57a1e4872d66a11fd5a4d10d38fa6ca49ad
P0n3y4lb1n0s <gabcena44@gmail.com>
2024-04-04 15:51:24 +0200

    ajout contenu de la page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,21 @@
 
 
             @include('dashboard.navigation')
-            @include('components/sidebar-dashboard')
 
-            <!-- Page Content -->
+            <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+                @include('components/sidebar-dashboard')
+                <div class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90" id="sidebarBackdrop"></div>
+                <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+                    <main>
+{{--                        <div id="flash" class="relative z-50">--}}
+{{--                            <%= render "shared/flash" %>--}}
+{{--                        </div>--}}
+                        {{$slot}}
+                    </main>
+                </div>
+            </div>
+
+
 
 
         <script>
