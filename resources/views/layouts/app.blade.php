0330403e66c43a7c7626ec59e587bb8e5301c04a
P0n3y4lb1n0s <gabcena44@gmail.com>
2024-04-04 10:15:14 +0200

    positionemen navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,8 @@
     </head>
     <body class="bg-gray-50 dark:bg-gray-900">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @include('dashboard.navigation')
             @include('components/sidebar-dashboard')
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
