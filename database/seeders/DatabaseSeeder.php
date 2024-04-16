7de817d293bd038788b1deb8608c905b081db345
P0n3y4lb1n0s <gabcena44@gmail.com>
2024-04-16 12:15:05 +0200

    modification du seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            RoleSeeder::class,
             PermissionSeeder::class,
+            RoleSeeder::class,
         ]);
     }
 }
