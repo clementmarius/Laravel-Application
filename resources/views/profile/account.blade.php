ec81453d47485e66ef7bb90d126b46be6f9781c1
P0n3y4lb1n0s <gabcena44@gmail.com>
2024-04-17 16:14:15 +0200

    supression d'un guillemet

--- resources/views/profile/account.blade.php
+++ resources/views/profile/account.blade.php
@@ -39,7 +39,7 @@
                                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                             <path clip-rule="evenodd" fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"></path>
                                         </svg>
-                                        <span class="leading-tight">Email can be edited on the <a href="/dashboard/account/email/edit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">"email edition page</a></span>
+                                        <span class="leading-tight">Email can be edited on the <a href="/dashboard/account/email/edit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">email edition page</a></span>
                                     </li>
                                     <li class="flex space-x-2">
                                         <!-- Icon -->
