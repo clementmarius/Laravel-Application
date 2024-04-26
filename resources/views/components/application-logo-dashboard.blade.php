07a19d29c44be66f6a35a7682f258d461c7a67b7
marius <mariusclement68@gmail.com>
2024-04-26 14:18:48 +0200

    updated logo dashboard

---
+++ resources/views/components/application-logo-dashboard.blade.php
@@ -0,0 +1,3 @@
+<a href="/" class="gap-2 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+    <img src="{{URL::asset('/images/logo.png')}}" class="w-8 h-8 mr-2" alt="logo" > SaasLit
+</a>
--- resources/views/dashboard/navigation.blade.php
+++ resources/views/dashboard/navigation.blade.php
@@ -2,14 +2,7 @@
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
             <div class="gap-2 flex items-center justify-start">
-{{--                <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar" class="p-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">--}}
-{{--                    <svg id="toggleSidebarMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">--}}
-{{--                        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>--}}
-{{--                    </svg>--}}
-{{--                    <svg id="toggleSidebarMobileClose" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">--}}
-{{--                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>--}}
-{{--                    </svg>--}}
-{{--                </button>--}}
+
 
                 <button id="toggleSidebarMobile" data-drawer-target="default-sidebar" data-drawer-backdrop="false" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="p-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     <span class="sr-only">Open sidebar</span>
@@ -20,7 +13,9 @@
                                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                         </svg>
                 </button>
-                @include('components/logo')
+                <div class="h-8 mr-3 px-2.5">
+                    @include('components/application-logo-dashboard')
+                </div>
             </div>
             <div class="flex items-center">
                 <div class="hidden mr-3 -mb-1 sm:block">
